1c273db6d9123724c645c9093b0f2609068ff86f
Kesiah Patil <kesiahpatil@Kesiahs-MacBook-Pro.local>
2020-11-01 14:38:37 -0500

    added prompts for user to input password length and character types

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,31 @@
 // Assignment code here
+//Function to get ammount of characters
+var getCharacters = function() {
+  var characters = window.prompt("How many characters would you like your password to contain?");
+  if((characters > 7) && (characters < 129)) {
+    return characters;
+  }
+  else {
+    window.alert("Please enter a numeric value between 8 and 128");
+    getCharacters();
+  }
+}
+//Generate Password Function
+var generatePassword = function() {
+  passLength = getCharacters();
+  var confirmLower = window.confirm("Would you like to include lowercase letters in your password?");
+  var confirmUpper = window.confirm("Would you like to include Uppercase letters in your password?");
+  var confirmNumber = window.confirm("Would you like to include numbers in your password?");
+  var confirmSpecial = window.confirm("Would you like to include special characters in your password?");
 
+  var typeAmmount = [confirmLower, confirmUpper, confirmNumber, confirmSpecial];
+  console.log(confirmLower + confirmUpper + confirmNumber + confirmSpecial);
+  for (i = 0; i < passLength; i++)
+    if(confirmLower) {
+      
+  }
+  
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
